16270fc517ffacf1e48717b1d3b4a4fc1a284928
sreerag1185 <sreerag1185@gmail.com>
2023-10-27 10:29:41 +0530

    travel

---
+++ travelproject/travelapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import travel
+from .models import travel_end
+
+# Register your models here.
+admin.site.register(travel)
+admin.site.register(travel_end)
